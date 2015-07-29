c109c40ca32822d23598415a71c3927f077b9be4
Ruben d'Arco <cyclops-github@prof-x.net>
2015-07-29 08:23:07 -0700

    Documentation and versioning

--- K2Field.K2NE.ServiceBroker/Properties/AssemblyInfo.cs
+++ K2Field.K2NE.ServiceBroker/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersionAttribute("1.3.1.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.4.0.0")]
