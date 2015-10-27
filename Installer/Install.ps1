9a64fcf18fe899d15f4302696d1c442f17b0cbf8
Ruben d'Arco <cyclops-github@prof-x.net>
2015-10-27 09:10:16 -0700

    Small changes to powershell installer.

--- Installer/Install.ps1
+++ Installer/Install.ps1
@@ -1,27 +1,36 @@
-#This GUID ** MUST BE ** unique. If you doubt, make a new one. Do NOT reuse that one !!!
 #If a GUID is found that is the same, it will "overwrite" the service type.
-$guid = [guid]"11e82457-e2db-46d9-b387-398bea0a610d" 
+$guid = [guid]"1cf912ee-0d6d-43a8-b8b5-9514ac30113e" 
 $brokerName = "K2Field.K2NE.ServiceBroker"
 $brokerSystemName = $brokerName
-$brokerDLL = "$($brokerName).dll" #Make sure that matched the name of your file
-$displayName = $brokerName -replace '\.',' ' #you can be creative here if you prefer...
-$brokerDescription = "K2NE Service Broker's description."
+$brokerDLL = "$($brokerName).dll"
+$displayName = $brokerName -replace '\.',' '
+$brokerDescription = "The K2NE Service Broker. For more information, see https://github.com/K2NE/K2NEServiceBroker"
 
-Function StartK2Service([string]$server) {
-		Write-Host -ForegroundColor DarkMagenta "Starting K2 blackpearl service on $server"
-		Get-Service -DisplayName 'K2 blackpearl Server' -ComputerName $server | where-object {$_.Status -eq "Stopped"} | Start-Service
+Function StartK2Service() {
+	Write-Host  "Starting K2 blackpearl service"
+	$job = Start-Job -ScriptBlock {
+		Get-Service -DisplayName 'K2 blackpearl Server' | where-object {$_.Status -ne "Running"} | Start-Service
+	}
+	Wait-Job $job
+	Receive-Job $job
 }
 
-Function StopK2Service([string]$server) {
-    Write-Host -ForegroundColor DarkMagenta "Stopping K2 blackpearl service on $server"
-    Get-Service -DisplayName 'K2 blackpearl Server' -ComputerName $server | where-object {$_.Status -eq "Running"} | Stop-Service
+Function StopK2Service() {
+    Write-Host "Stopping K2 blackpearl service"
+
+	$job = Start-Job -ScriptBlock {
+		Get-Service -DisplayName 'K2 blackpearl Server' | where-object {$_.Status -eq "Running"} | Stop-Service -Confirm:$false -Force 
+		Stop-Process -ProcessName "K2HostServer"  -force -ErrorAction SilentlyContinue -Confirm:$false
+	}
+	Wait-Job $job
+	Receive-Job $job
 }
 
 Function GetK2InstallPath([string]$machine = $env:computername) {
     $registryKeyLocation = "SOFTWARE\SourceCode\BlackPearl\blackpearl Core\"
     $registryKeyName = "InstallDir"
 
-		Write-Debug "Getting K2 install path from $machine "
+	Write-Debug "Getting K2 install path from $machine "
     
     $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $machine)
     $regKey= $reg.OpenSubKey($registryKeyLocation)
@@ -29,19 +38,15 @@ Function GetK2InstallPath([string]$machine = $env:computername) {
     return $installDir
 }
 
-Function GetK2ConnectionString([string]$k2Server = "localhost", [int]$port = 5555, [string]$userid, [string]$password, [string]$windowsdomain, [bool]$integrated = $true, [bool]$isPrimary=$true) {
+Function GetK2ConnectionString([string]$k2Server = "localhost", [int]$port = 5555) {
     Write-Debug "Creating connectionstring for machine '$k2Server' and port '$port'";
-	
-		Add-Type -AssemblyName ('SourceCode.HostClientAPI, Version=4.0.0.0, Culture=neutral, PublicKeyToken=16a2c5aaaa1b130d')
+	Add-Type -AssemblyName ('SourceCode.HostClientAPI, Version=4.0.0.0, Culture=neutral, PublicKeyToken=16a2c5aaaa1b130d')
 		
     $connString = New-Object -TypeName "SourceCode.Hosting.Client.BaseAPI.SCConnectionStringBuilder";
-    $connString.Integrated = $integrated;
-    $connString.IsPrimaryLogin = $isPrimary;
+    $connString.Integrated = $true;
+    $connString.IsPrimaryLogin = $true;
     $connString.Host = $k2Server;
     $connString.Port = $port;
-		$connString.UserID = $userid;
-		$connString.Password = $password;
-		$connString.WindowsDomain = $windowsdomain;
 
     return $connString.ConnectionString; 
 }
@@ -52,8 +57,10 @@ Function RegisterServiceType([string]$k2ConnectionString, [string]$k2Server, [st
     $remK2Path = GetK2InstallPath -machine $k2Server
     $smoManServiceAssembly = Join-Path $k2Path "bin\SourceCode.SmartObjects.Services.Management.dll"
     $serviceBrokerAssembly = Join-Path $remK2Path "ServiceBroker\$($ServiceTypeDLL)"
+	$className = "K2Field.K2NE.ServiceBroker.K2NEServiceBroker";
 	
     Write-Debug "Adding/Updating ServiceType $serviceBrokerAssembly with guid $guid using $k2ConnectionString"
+	Write-Debug  "ServiceBrokerAssembly: $serviceBrokerAssembly"
     
     Add-Type -Path $smoManServiceAssembly #We load this assembly locally, but we connect to the remote host.
     $smoManService = New-Object SourceCode.SmartObjects.Services.Management.ServiceManagementServer
@@ -66,11 +73,11 @@ Function RegisterServiceType([string]$k2ConnectionString, [string]$k2Server, [st
     # Check if we need to update or register a new one...
     if ([string]::IsNullOrEmpty($smoManService.GetServiceType($guid)) ) {
         Write-Debug "Registering new service type..."
-        $tmpOut = $smoManService.RegisterServiceType($guid, $systemName, $displayName, $description, $serviceBrokerAssembly, $systemName);
+        $tmpOut = $smoManService.RegisterServiceType($guid, $systemName, $displayName, $description, $serviceBrokerAssembly, $className);
         write-debug "Registered new service type..."
     } else {
         Write-Debug "Updating service type..."
-        $tmpOut = $smoManService.UpdateServiceType($guid, $systemName, $displayName, $description, $serviceBrokerAssembly, $systemName);
+        $tmpOut = $smoManService.UpdateServiceType($guid, $systemName, $displayName, $description, $serviceBrokerAssembly, $className);
         Write-Debug "Updated service type..."
     }
     $smoManService.Connection.Close();
@@ -80,18 +87,11 @@ Function RegisterServiceType([string]$k2ConnectionString, [string]$k2Server, [st
 $targetPath = GetK2InstallPath
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
-Write-Host Stopping Service
-StopK2Service -server localhost
-sleep -Seconds 2
- 
-Write-Host Installing files
+StopK2Service -server localhost 
 Copy-Item $scriptPath\* -Include K2Field.K2NE.ServiceBroker.dll -Destination "$targetPath\ServiceBroker"
-sleep -Seconds 2
-
-Write-Host Starting Service
 StartK2Service -server localhost
-sleep -Seconds 2
+
 
 $k2constring = GetK2ConnectionString 
 RegisterServiceType -k2ConnectionString $k2constring -ServiceTypeDLL $brokerDLL -guid $guid -systemName $brokerSystemName -displayName $displayName -description $brokerDescription
-sleep -Seconds 2
+
