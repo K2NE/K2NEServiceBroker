81ed588ab14f80de0cf699cb7e5c9e1866a2756b
Ruben d'Arco <cyclops-github@prof-x.net>
2015-12-29 06:15:48 -0800

    Version numbering on every build.
    Fixes #71

---
+++ Solution Files/UpdateBuildNr.ps1
@@ -0,0 +1,10 @@
+$newVersion = get-date -Format "yyyyMMddHHmm"
+
+$AssemInfo = split-path -Path $PSScriptRoot -parent
+$AssemInfo += "\K2Field.K2NE.ServiceBroker\Properties\AssemblyInfo.cs"
+
+$TmpFile = $AssemInfo + ".tmp"
+get-content $AssemInfo | %{$_ -replace 'AssemblyFileVersion\(\"([0-9]\.[0-9]\.[0-9])\.([0-9]*)\"\)',"AssemblyFileVersion(""`$1.$newVersion"")" } > $TmpFile
+
+
+Move-item $TmpFile $AssemInfo -force
