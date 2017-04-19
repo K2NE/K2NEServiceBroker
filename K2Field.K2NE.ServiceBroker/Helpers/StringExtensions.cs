ea664795123b2866a805cdf58d85541b84ae8523
Ruben d'Arco <cyclops-github@prof-x.net>
2017-04-18 23:44:20 -0700

    Add missing stringextensions

---
+++ K2Field.K2NE.ServiceBroker/Helpers/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace K2Field.K2NE.ServiceBroker.Helpers
+{
+    public static class StringExtensions
+    {
+        public static string ToStringOrEmpty(this Object value)
+        {
+            if (value == null)
+            {
+                return String.Empty;
+            }
+            else
+            {
+                return value.ToString(); 
+            }
+        }
+    }
+}
